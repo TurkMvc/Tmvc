097239601c2931693d379c069766279b16227d51
Ali GOREN <goren.ali@yandex.com>
2018-03-29 11:36:38 +0300

    Project extracted

---
+++ app/Routes/routes.php
@@ -0,0 +1,12 @@
+<?php
+$router = new AltoRouter;
+$router->map('GET', '/', 'App\Controllers\IndexController@show', 'home');
+
+/*
+$router->map('GET', '/featured', 'App\Controllers\IndexController@featuredProducts', 'feature_product');
+$router->map('GET', '/get-products', 'App\Controllers\IndexController@getProducts', 'get_product');
+$router->map('POST', '/load-more', 'App\Controllers\IndexController@loadMoreProducts', 'load_more_product');
+
+$router->map('GET', '/product/[i:id]', 'App\Controllers\ProductController@show', 'product');
+$router->map('GET', '/product-details/[i:id]', 'App\Controllers\ProductController@get', 'product_details');
+*/
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>TurkMvc E-Commerce Store - @yield('title')</title>
+</head>
+<body>
+	
+</body>
+</html>
